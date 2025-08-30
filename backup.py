a4d368af161ad330b2437a9ef0b3ef10f799e5c7
Agustin Bustos Piasentini (Nitro) <63634574+abustosp@users.noreply.github.com>
2025-08-30 14:46:23 -0300

    Add functionality to create backups of specified directories

--- backup.py
+++ backup.py
@@ -35,6 +35,26 @@ def crear_backup(exclude_dirs: list = None):
         comando = f"tar -zcf backup/{nombre_archivo} {carpeta}"
         os.system(comando)
         
+def crear_backup_incluidos(include_dirs: list):
+    """
+    Crea un backup solo de los directorios especificados en include_dirs.
+    Los archivos de backup se guardan en formato tar.gz y se les agrega la fecha actual.
+
+        :param include_dirs: Lista de directorios a incluir en el backup.
+    """
+    fecha_actual = date.today().strftime('%Y-%m-%d')
+    os.makedirs('backup', exist_ok=True)
+
+    # Filtrar solo los directorios existentes
+    carpetas_validas = [c for c in include_dirs if os.path.isdir(c)]
+
+    for carpeta in carpetas_validas:
+        print(carpeta)
+        nombre_archivo = f"{carpeta}_{fecha_actual}.tar.gz"
+        comando = f"tar -zcf backup/{nombre_archivo} {carpeta}"
+        os.system(comando)
+        
 if __name__ == "__main__":
     # Excluir el directorio 'backup' del backup
-    crear_backup(exclude_dirs=['backup'])
+    #crear_backup(exclude_dirs=['backup'])
+    crear_backup_incluidos(include_dirs=['dir1', 'dir2'])  # Reemplaza con los nombres de los directorios que deseas incluir
